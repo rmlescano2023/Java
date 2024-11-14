f111b411b3499e1c605923277ec767ff3a93fd82
Renmar M. Lescano <135441595+rmlescano2023@users.noreply.github.com>
2024-11-14 14:00:16 +0800

    Delete Bingo Sample directory

--- Bingo Sample/Fill.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public interface Fill {
-    public void fillArrayList(ArrayList<Integer> cardValues, int start);
-}
--- Bingo Sample/GameSystem.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-import java.util.Random;
-
-public interface GameSystem {
-    public int action(ArrayList<Integer> cardValues, Random random, int bingoNum);
-}
